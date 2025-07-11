605270642b36e1ceae346fd8baf805a68307b6fa
ihsandedec <ddcihsan@gmail.com>
2025-07-12 00:49:46 +0300

    Read the database structure in the sigortamnet.db file with scaffold and created EF Core classes (Model + DbContext)

---
+++ Backend/Data/SigortamNetDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data;
+
+public partial class SigortamNetDbContext : DbContext
+{
+    public SigortamNetDbContext()
+    {
+    }
+
+    public SigortamNetDbContext(DbContextOptions<SigortamNetDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlite("Data Source=sigortamnet.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasIndex(e => e.Tckn, "IX_Customers_TCKN").IsUnique();
+
+            entity.Property(e => e.CreatedDate).HasDefaultValueSql("datetime('now')");
+            entity.Property(e => e.IsActive).HasDefaultValue(1);
+            entity.Property(e => e.Tckn).HasColumnName("TCKN");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Backend/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Backend.Models;
+
+public partial class Customer
+{
+    public int Id { get; set; }
+
+    public string Tckn { get; set; } = null!;
+
+    public string Ad { get; set; } = null!;
+
+    public string Soyad { get; set; } = null!;
+
+    public string DogumTarihi { get; set; } = null!;
+
+    public int IsActive { get; set; }
+
+    public string CreatedDate { get; set; } = null!;
+}
